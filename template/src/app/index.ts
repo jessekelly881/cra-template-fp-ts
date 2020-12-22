40787cb978f45d90a6e024ea68686dc0311e5bf4
Jesse Kelly <kellyjesse881@gmail.com>
2020-12-21 19:28:18 -0600

    feat: updated src and tests

--- template/src/app/index.ts
+++ template/src/app/index.ts
@@ -2,10 +2,11 @@ import { tags } from "react-ts-fns";
 const { _, h1, p } = tags;
 
 function App() {
-    return _(null, [
-        h1(null, "Create React App Fp-ts Template"),
-        p(null, "cra-template-fp-ts"),
-    ]);
+    return _(
+        {},
+        h1({}, "Create React App Fp-ts Template"),
+        p({}, "cra-template-fp-ts"),
+    );
 }
 
 export default App;
--- template/src/app/tests/index.test.ts
+++ template/src/app/tests/index.test.ts
@@ -3,6 +3,6 @@ import App from "..";
 
 test("renders learn react link", () => {
     render(App());
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const bodyEl = screen.getByText(/cra-template-fp-ts/i);
+    expect(bodyEl).toBeInTheDocument();
 });
